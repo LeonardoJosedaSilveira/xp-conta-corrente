f46724f3abb04d4b89dca7053d0a245ad1dfb038
Leonardo José da Silveira <rpgleo.js@gmail.com>
2022-02-05 16:15:08 -0300

    create account - credit

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('POST /account', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,10 +15,130 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('criando conta', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/account')
+      .expect(201)
+      .expect('Content-Type', /json/);
   });
 });
+
+describe('POST /bank-transition/credit/2', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('acionar valor de 100  (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/bank-transition/credit/2')
+      .send({
+        "value":  100,
+        "origin": "banco-agencia-conta-titular"
+      })
+      .expect(201, {
+        "message": "The amount of 100 has been added to account 2"
+      })
+      .expect('Content-Type', /json/);
+
+  });
+
+  it('usuario invalido  (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/bank-transition/credit/9992035198165481')
+      .send({
+        "value":  100,
+        "origin": "banco-agencia-conta-titular"
+      })
+      .expect(400, {
+        "statusCode": 400,
+        "message": "Account not exist",
+        "error": "Bad Request"
+      })
+      .expect('Content-Type', /json/);
+
+  });
+
+  it('valor invalido  (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/bank-transition/credit/2')
+      .send({
+        "value":  -900,
+        "origin": "banco-agencia-conta-titular"
+      })
+      .expect(400, {
+        "statusCode": 400,
+        "message": "Invalid value",
+        "error": "Bad Request"
+      })
+      .expect('Content-Type', /json/);
+
+  });
+});
+
+
+describe('POST /bank-transition/debit/2', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('debita valor de 5  (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/bank-transition/debit/2')
+      .send({
+        "value":  5,
+        "origin": "banco-agencia-conta-titular"
+      })
+      .expect(201, {
+        "message": "The amount of 5 was debited from account 2"
+      })
+      .expect('Content-Type', /json/);
+
+  });
+
+  it('usuario invalido  (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/bank-transition/debit/9992035198165481')
+      .send({
+        "value":  5,
+        "origin": "banco-agencia-conta-titular"
+      })
+      .expect(400, {
+        "statusCode": 400,
+        "message": "Account not exist",
+        "error": "Bad Request"
+      })
+      .expect('Content-Type', /json/);
+
+  });
+
+  it('valor invalido  (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/bank-transition/debit/2')
+      .send({
+        "value":  -900,
+        "origin": "banco-agencia-conta-titular"
+      })
+      .expect(400, {
+        "statusCode": 400,
+        "message": "Invalid value",
+        "error": "Bad Request"
+      })
+      .expect('Content-Type', /json/);
+
+  });
+});
+
